c6116dc1fea510f7f8cbd02077b61564fcc5892a
yavuz selim karataş <yavuzselimkaratas@gmail.com>
2022-03-07 01:58:33 +0300

    Initial Commit

---
+++ Project8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by Yavuz Selim Karataş on 7.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
